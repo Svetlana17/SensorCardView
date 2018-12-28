2be3bc419c197d5b056f31d32cb9dc2adbe13395
Users <lana.chebotareva.95@mail.ru>
2018-12-28 20:26:13 +0300

    Add start activity

---
+++ app/src/main/java/com/example/user/fragmentexsemple/StartActivity.java
@@ -0,0 +1,23 @@
+package com.example.user.fragmentexsemple;
+
+import android.content.Intent;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class StartActivity extends AppCompatActivity implements  View.OnClickListener {
+    Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+        button = (Button) findViewById(R.id.button);
+            button.setOnClickListener((View.OnClickListener) this);
+    }
+            public void onClick (View v){
+            Intent intent = new Intent(StartActivity.this, MainActivity.class);
+            startActivity(intent);
+        }
+    }
